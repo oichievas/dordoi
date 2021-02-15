b25bf1f1838a491ecfb32da9dc89565c3fff5f3e
Bekbolot Tazhibaev <tabekg@gmail.com>
2021-02-16 01:40:16 +0600

    fix ui (#3)

--- js/product_card.js
+++ js/product_card.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
-	
+
 	$('.card').delay(1800).queue(function(next) {
 		$(this).removeClass('hover');
 		$('a.hover').removeClass('hover');
 		next();
 	});
+
+	$('.slider').css('background-position', '0px ' + 190 + 'px');
+
+	$('#nav_categories_dropdown_container').hover(function(e){
+		$('#nav_categories_dropdown_container').dropdown('show');
+	}, function(e){
+		$('#nav_categories_dropdown_container').dropdown('hide');
+	});
+
+	$('#nav_categories_link').on('click', function() {
+		location.href = $(this).attr('href');
+	});
+
 });
