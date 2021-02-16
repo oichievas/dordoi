d7f8fae172b178d1313f3182031a4f04543e045c
edzen12 <oichiev.edzen@gmail.com>
2021-02-16 14:56:44 +0600

    dev branch, add breadcrumbs

--- js/product_card.js
+++ js/product_card.js
@@ -18,4 +18,19 @@ $(document).ready(function() {
 		location.href = $(this).attr('href');
 	});
 
+
+	// Breadcrums
+	$('.second ol li a.active-2 span').css({ 'border-bottom': '3px solid rgb(0, 183, 255) ' , 'padding-bottom': ' 21px ' });
+
+       $('.second ol li a').click(function() {
+       $('.second ol li a.active-2 span').css( 'padding-bottom', '0px' );
+       $('.second ol li a.active-2 span').css( 'border-bottom', 'none' );
+       $('.second ol li a.active-2').removeClass('active-2');
+       $(this).closest('a').find('span').css( 'padding-bottom', ' 21px ' );
+       $(this).closest('a').find('span').css( 'border-bottom', '3px solid rgb(0, 183, 255) ' );
+       $(this).closest('a').addClass('active-2');
+    });
+
 });
+
+
